8722e091a66fdcb61a0ff2ddd6ce81350ca69541
Chuni <paggy881025@gmail.com>
2026-06-11 23:55:24 +0800

    完成friend-page切版

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import FriendsPage from '../components/FriendsPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/friends-page',
+      name: 'friends-page',
+      component: FriendsPage,
+    },
+  ],
 })
 
 export default router
