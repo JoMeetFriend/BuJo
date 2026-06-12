6e4dce968e4ed86ebebb5c3218168ded42600884
f22212225 <f22212225@gmail.com>
2026-06-12 10:11:46 +0800

    補存檔index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 /*登入頁面*/
-import LoginView from "../component/LoginView.vue"
+import LoginView from "../components/LoginView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
