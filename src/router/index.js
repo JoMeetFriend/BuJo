0b189c9d807496b88d4b5c209fc33d7a4e403987
Chuni <paggy881025@gmail.com>
2026-06-17 15:39:14 +0800

    feat:新增登入才能看到畫面

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventView from '../components/EventView.vue'
+import { useAuthStore } from '@/stores/auth'
 
 /*登入頁面*/
 import LoginView from '../components/LoginView.vue'
@@ -9,12 +10,15 @@ import FriendsPage from '../components/FriendsPage.vue'
 import AvailabilityPickerModal from '../components/AvailabilityPickerPreview.vue'
 
 const router = createRouter({
+
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'calendar-page',
       component: () => import('../components/CalendarMain.vue'),
+      // 有meta: { requiresAuth: true }的部分需要登入才看得到頁面
+      meta: { requiresAuth: true }
     },
     {
       path: '/profile/edit',
@@ -60,6 +64,16 @@ const router = createRouter({
       component: AvailabilityPickerModal,
     },
   ],
+})
+
+// 有meta: { requiresAuth: true }的部分需要登入才看得到頁面
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore()
+  if (to.meta.requiresAuth && !authStore.isLoggedIn) {
+    next('/login')
+  } else {
+    next()
+  }
 })
 
 export default router
