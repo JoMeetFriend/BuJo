e80c227b9e12dbe4e901218706a90797d8369387
yu <271432374+yuaangela@users.noreply.github.com>
2026-06-25 16:21:13 +0800

    chore: 更新 apiClient 的環境變數名稱

--- src/stores/friendStore.js
+++ src/stores/friendStore.js
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia'
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 5000,
 })
 
